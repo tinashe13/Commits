3a53d4b6085dcde8babedd3106ba5cfa1aa2588b
Tinashe <tinashedzemwa300@gmail.com>
2025-03-27 13:48:42 -0500

    version working

---
+++ routes/auth.js
@@ -0,0 +1,47 @@
+import express from "express";
+import { Register, Login, Logout } from "../controllers/auth.js";
+import Validate from "../middleware/validate.js";
+import { check } from "express-validator";
+
+const router = express.Router();
+
+// Register route -- POST request
+router.post( "/register",
+    check("email")
+        .isEmail()
+        .withMessage("Enter a valid email address")
+        .normalizeEmail(),
+    check("first_name")
+        .not()
+        .isEmpty()
+        .withMessage("You first name is required")
+        .trim()
+        .escape(),
+    check("last_name")
+        .not()
+        .isEmpty()
+        .withMessage("You last name is required")
+        .trim()
+        .escape(),
+    check("password")
+        .notEmpty()
+        .isLength({ min: 8 })
+        .withMessage("Must be at least 8 chars long"),
+    Validate,
+    Register
+);
+
+router.post( "/login",
+    check("email")
+        .isEmail()
+        .withMessage("Enter a valid email address")
+        .normalizeEmail(),
+    check("password").not().isEmpty(),
+    Validate,
+    Login
+);
+
+router.get('/logout', Logout);
+
+export default router;
+
